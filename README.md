91f6577147235266ff7a12b312a360184e6320c0
ou1z <itzou1z@gmail.com>
2020-11-08 21:07:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Credits
+
+Discord: cj#1211
+Github: https://www.github.com/ou1z
+V3rmillion: ou1z
---
+++ extension.js
@@ -0,0 +1,73 @@
+const vscode = require('vscode');
+const fs = require('fs')
+const { exec } = require('child_process')
+const luamin = require('./luamin.js')
+
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+
+let dump = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left)
+dump.command = 'psudumper.constantdump'
+dump.tooltip = "Gets the constants for a PSU obfuscated script."
+dump.text = 'Constant Dump PSU'
+
+function getEditorText() {
+	const editor = vscode.window.activeTextEditor;
+
+	if (editor) {
+		let document = editor.document;
+		const documentText = document.getText();
+
+		return documentText
+	}
+}
+function replaceAll(string, search, replace) {
+	return string.split(search).join(replace);
+}
+
+function activate(context) {
+
+	dump.show()
+
+	let disposable = vscode.commands.registerCommand('psudumper.constantdump', function () {
+		let scripttodump = getEditorText()
+
+
+		let header = `-----------------------------\nSuccessfully Constant Dumped!\nDate: ${new Date()}\n-----------------------------\n\n\n`
+
+
+		let script = luamin.Beautify(scripttodump, {
+			RenameGlobals: false,
+			RenameVariables: false,
+			SolveMath: false
+		})
+	
+
+		let deserializer = script.substring(script.lastIndexOf('break')-26, script.lastIndexOf('break')-18)
+		let newscript = replaceAll(script, deserializer, deserializer + `print(${deserializer.charAt(deserializer.length-2)})`)
+
+		fs.writeFileSync('./runscript.lua', newscript)
+		exec('lua ./runscript.lua', (err,stdout,stderr) => {
+			vscode.workspace.openTextDocument({'content': header + stdout})
+			fs.unlinkSync('./runscript.lua')
+		})
+
+
+		vscode.window.showInformationMessage('Dumped!')
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+exports.activate = activate;
+function deactivate() {
+	dump.dispose()
+}
+
+module.exports = {
+	activate,
+	deactivate
+}
